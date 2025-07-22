5f66c0f913bbe88d1bc4c19419d6521445e1e9a3
shreyasvemula <vemulashreyas5@gmail.com>
2025-07-22 14:53:42 +0530

    Initial commit

---
+++ chp 1.py
@@ -0,0 +1,26 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+
+# Load dataset
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# Normalize pixel values
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# One-hot encode labels
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# Define model
+model = Sequential([
+    Flatten(input_shape=(32, 32, 3)),  # Correctly flattens 32x32x3 into 3072
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')  # 10 classes for CIFAR-10
+])
+
+# Compile and train
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=10, batch_size=64)
